357596eeb737c9d007b5bb26540de0c97871aed6
pavlig43 <pavlig43@gmail.com>
2023-03-06 20:56:46 +0300

    доделан магнит, нужно добавить сессию

--- agrokomplex/main_agrokomplex.py
+++ agrokomplex/main_agrokomplex.py
@@ -1,10 +1,13 @@
 import re
 
 import pandas as pd
+import requests
 from bs4 import BeautifulSoup
+from pandas import DataFrame
+from fake_useragent import UserAgent
 
 from catalog import catalog
-from sort_df import sort_df
+
 
 
 def agrok():
@@ -12,11 +15,10 @@ def agrok():
     links = []
     prices_sales = []
     for i in range(1, 12):
-        # url = f'https://agrokomplexshop.ru/landings/all_skidki/?PAGEN_2={i}&ajax_get=Y&AJAX_REQUEST=Y&bitrix_include_areas=N'
-        # page_get = requests.get(url)
-        # page = BeautifulSoup(page_get.text, 'html.parser')
-        html_page = open(f'Html_lst/page{i}.html', 'r')
-        page = BeautifulSoup(html_page, 'html.parser')
+        url = f'https://agrokomplexshop.ru/landings/all_skidki/?PAGEN_2={i}&ajax_get=Y&AJAX_REQUEST=Y&bitrix_include_areas=N'
+        headers = {'User-Agent': UserAgent().chrome}
+        page_get = requests.get(url,headers=headers)
+        page = BeautifulSoup(page_get.text, 'html.parser')
         prices_sales_block = page.find_all('div', {'class': 'price_matrix_wrapper'})
         for i in prices_sales_block:  # блок с ценами и скидками
             price = i.find('div', {'class': 'price font-bold font_mxs'}).text.replace('\n', '')
@@ -33,11 +35,16 @@ def agrok():
 
     names = [name.text for name in names]
     links = [f"https://agrokomplexshop.ru{link.get('href')}" for link in links]
-    agrokomplex_df = pd.DataFrame({'Наименование': names,
+    agrokomplex_df: DataFrame = pd.DataFrame({'Наименование': names,
                                    'Ссылка': links,
                                    'Цена': [i[0] for i in prices_sales],
                                    'Скидка': [i[1] for i in prices_sales]})
-    sort_df(agrokomplex_df, catalog)
+
+
+    agrokomplex_df = agrokomplex_df[agrokomplex_df.Ссылка.str.contains('|'.join(catalog))]#отбор из каталога нужных категорий
+    agrokomplex_df = agrokomplex_df.sort_values(by='Скидка', ascending=False)
+    agrokomplex_df = agrokomplex_df.drop_duplicates(keep='first')
+    agrokomplex_df.to_excel('1.xlsx')
 if __name__ == '__main__':
     agrok()
 
--- sort_df.py
+++
@@ -1,6 +0,0 @@
-def sort_df(name_df, catalog=''):
-    if catalog:
-        name_df = name_df[name_df.Ссылка.str.contains('|'.join(catalog))] #отбор товаров из каталога
-    name_df = name_df.sort_values(by='Скидка', ascending=False)
-    name_df = name_df.drop_duplicates(keep='first')
-

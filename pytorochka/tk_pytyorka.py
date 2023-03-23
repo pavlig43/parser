59fa4924a13f43bf6f4f5eab68410b6ac855f0ba
pavlig43 <pavlig43@gmail.com>
2023-03-23 22:23:54 +0300

    сделал тк выбор

--- pytorochka/tk_pytyorka.py
+++ pytorochka/tk_pytyorka.py
@@ -39,54 +39,66 @@ def tk_category():
     # root.mainloop()
 
 
-def see_category(dict_shop):
+def get_json_category(dict_shop):# передаю словарь с категориями , полученные с сайта
     root = Tk()
     root.geometry('600x900')
     root.title('1313')  # имя магазина
     tree = CheckboxTreeview(root, columns='value')
-    vert_scroll = ttk.Scrollbar(root, orient='vertical', command=tree.yview)
+    vert_scroll = ttk.Scrollbar(root, orient='vertical', command=tree.yview)# ползунок вертикальный
     vert_scroll.pack(side='right', fill='y')
     tree.configure(height=40, yscrollcommand=vert_scroll.set)
-    men = {}
+    with open('my_choice.json', 'r') as file:# выгружаю из файла мой выбор
+        my_choice = file.read()
+    my_choice = json.loads(my_choice)
 
     for category in dict_shop:
 
         category_row = tree.insert("", 1, text=category)  # основные категории
         for sub_name, value in dict_shop[category].items():
             id2 = tree.insert(category_row, "end", text=sub_name, values=value)
-            if sub_name in men:  # проверка на наличие вмоем выборе
-                tree.item_check(id2)
-            else:
-                tree.item_uncheck(id2)
+            if category in my_choice.keys():
+                if sub_name in my_choice[category]:  # проверка категорий с сайта на наличие в моем выборе
+                    tree.item_check(id2) #ставлю галочки
+                else:
+                    tree.item_uncheck(id2)
 
-    def entered(event):
+    def get_my_choice(event):
+		#функция при нажатии кнопки получить остатки
+		#собирает все галочки и делает словарь и обновляет json с моим выбором
         my_choice = {}
         categories = tree.get_children()# список всех категорий в виджете
         for category in categories:
             subcategories = {}
-            for subcategory in tree.get_children(category): # ищу подкатегории
-                name = tree.item(subcategory)['text']  # имя , которое видит пользователь
-                value = tree.item(subcategory)['values'] # значение, которое к нему идет в словарь , используется для парсинга
+            for subcategory in tree.get_children(category): # ищу подкатегори
+                if 	tree.item(subcategory)['tags'] == ['checked']:
+                    name = tree.item(subcategory)['text']  # имя , которое видит пользователь
+                    value = tree.item(subcategory)['values'] # значение, которое к нему идет в словарь , используется для парсинга
+                    subcategories[name] = str(value[0])
 
-            subcategories[name] = str(value[0])
+                    my_choice[tree.item(category)['text']] = subcategories #основной словарь дополняю словарем подкатегории
+        my_choice_str = json.dumps(my_choice, ensure_ascii=False) # с русским текстом
+        with open('my_choice.json','w') as file:
+            file.write(my_choice_str)
 
-            my_choice[tree.item(category)['text']] = subcategories #основной словарь дополняю словарем подкатегории
+    btn = Button(text="Получить мой список")
 
-        my_lst = tree.get_checked()
-        # my_lst = [tree.item(i)['text'] for i in my_lst]
-        my_lst1 = [tree.item(i)['values'] for i in my_lst]
-        print(my_lst1)
-
-    btn = Button(text="Entered")
-
-    btn.bind("<ButtonPress>", entered)
-
-    tree.pack()
-    btn.pack()
+    btn.bind("<ButtonPress>", get_my_choice)# событие при нажатии кнопки , получаю мой выбор
+    checkbutton_var = BooleanVar() # создаю логическую переменую и привязываю ее к Checkbutton ниже
+    Checkbutton(root, text='Снять/выбрать всё', variable=checkbutton_var).pack(anchor='e')
+    def checked_all():                  # функция,которая проверяет состояние Checkbutton
+        for id in tree.get_children():  # и ставит или снимает галочки везде
+            if checkbutton_var.get():
+                tree.item_check(id)
+            else:
+                tree.item_uncheck(id)
+    checkbutton_var.trace('w',lambda *_:checked_all()) #метод trace при нажатии галочки,вызывает функцию выше
+    tree.pack(anchor='e')
+    btn.pack(anchor='e')
     root.mainloop()
+
 
 
 if __name__ == '__main__':
     a = get_all_category()
 
-    see_category(a)
+    get_json_category(a)
